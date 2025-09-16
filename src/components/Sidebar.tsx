677f645c1b95948ad0c9b23205e4b7caf3960bc7
Pleum <plemplemplemplem5@gmail.com>
2025-09-16 17:26:44 +0700

    lab14 done

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,10 +38,10 @@ export default function Sidebar() {
             color="red"
             withBorder
           >
-            <Avatar size="md" radius="xl" src="/charliebrown.jpg" />
+            <Avatar size="md" radius="xl" src="https://scontent.fcnx1-1.fna.fbcdn.net/v/t1.6435-9/117403631_656666138534223_1576702068341507713_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFe0MZ9SnWBBgmK5ZFBwbGwDkYStvv0BYcORhK2-_QFh-TbVRAs9ddApUuvA2CZS8rvPUpQIQ9UCkiB5E74gAgQ&_nc_ohc=W9vgETK3np8Q7kNvwFLumgU&_nc_oc=AdnaSlp5tUIzJjzGiO07OJmoajecYNUh1teEud0Hf7YF40aecQVerPcgiNQPNEbh27o&_nc_zt=23&_nc_ht=scontent.fcnx1-1.fna&_nc_gid=85MbI6iHw71admKdk_25Xw&oh=00_AfaNkKBYtgfQQhvquRZYHl5az8hLBY6hcPeEoTmhH02ZLA&oe=68F0B0F4" />
           </Indicator>
           <Text component={RouterNavLink} to="/">
-            User : Chanadda : Admin
+            User : Thanakorn : Student
           </Text>
         </Group>
       </Box>
